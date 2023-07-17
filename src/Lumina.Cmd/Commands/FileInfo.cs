dd6e9fda389c57efd63ba55330e344a5f8c2c470
Maple <8961085+MapleHinata@users.noreply.github.com>
2023-07-17 18:01:27 -0300

    Add lvb/sgb to Lumina.Cmd and shit out json

--- src/Lumina.Cmd/Commands/FileInfo.cs
+++ src/Lumina.Cmd/Commands/FileInfo.cs
@@ -1,10 +1,13 @@
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using Lumina.Data;
 using Lumina.Data.Files;
 using Lumina.Data.Files.Excel;
+using Newtonsoft.Json;
 
 namespace Lumina.Cmd.Commands
 {
@@ -40,10 +43,12 @@ namespace Lumina.Cmd.Commands
 
                 case ".lgb":
                     return gameData.GetFile< LgbFile >( path );
-
+                case ".lvb":
+                    return gameData.GetFile< LvbFile >( path );
+                case ".sgb":
+                    return gameData.GetFile< SgbFile >( path );
                 case ".tex":
                     return gameData.GetFile< TexFile >( path );
-                
                 case ".hwc":
                     return gameData.GetFile< HwcFile >( path );
             }
@@ -69,13 +74,80 @@ namespace Lumina.Cmd.Commands
                 case LgbFile f:
                 {
                     co.WriteLine( "Lgb Info:" );
+                    string fileDir = $"{f.FilePath.Path.Replace( '/', '-' )}";
+                    
+                    string chunkHeaderJson = JsonConvert.SerializeObject(f.ChunkHeader, Formatting.Indented);
+                    co.WriteLine( $" Scene Chunk Header: {chunkHeaderJson.Data()}");
+                    
                     co.WriteLine( $" layers: {f.Layers.Length.Data()}");
 
                     for( int i = 0; i < f.Layers.Length; i++ )
                     {
                         var layer = f.Layers[ i ];
                         co.WriteLine($" - layer {i.Data()} - {layer.Name.Data()}");
+                        string formattedJson = JsonConvert.SerializeObject(layer, Formatting.Indented);
+
+                        System.IO.Directory.CreateDirectory( fileDir );
+                        File.WriteAllText($"{fileDir}/{layer.Name}.json", formattedJson);
+                        co.WriteLine($"Layer: {formattedJson}");
+                        
                     }
+                    break;
+                }
+                case SgbFile f:
+                {
+                    co.WriteLine( "Sgb Info:" );
+                    string fileName = $"{f.FilePath.Path.Split( '/' ).Last()}.json";
+
+                    var sb = new StringBuilder();
+                    
+                    var chunkHeaderJson = JsonConvert.SerializeObject(f.ChunkHeader, Formatting.Indented);
+                    sb.AppendLine( chunkHeaderJson + ",");
+                    co.WriteLine( $" Scene Chunk Header: {chunkHeaderJson.Data()}");
+                    
+                    
+                    string layerGroupsJson = JsonConvert.SerializeObject(f.LayerGroups, Formatting.Indented);
+                    sb.AppendLine( layerGroupsJson + ",");
+                    co.WriteLine( $" Layer Groups: {layerGroupsJson.Data()}");
+                    
+                    string lgbAssetPaths = JsonConvert.SerializeObject(f.LgbAssetPaths, Formatting.Indented);
+                    sb.AppendLine( lgbAssetPaths + ",");
+                    co.WriteLine( $" LGB Asset Paths: {lgbAssetPaths.Data()}");
+                    
+                    
+                    string layerSetsJson = JsonConvert.SerializeObject(f.SGTimelines, Formatting.Indented);
+                    sb.AppendLine( layerSetsJson + ",");
+                    co.WriteLine( $" Layer Sets: {layerSetsJson.Data()}");
+                    
+                    File.WriteAllText(fileName, sb.ToString());
+                    break;
+                }
+                case LvbFile f:
+                {
+                    co.WriteLine( "Lvb Info:" );
+                    string fileName = $"{f.FilePath.Path.Split( '/' ).Last()}.json";
+
+                    var sb = new StringBuilder();
+                    
+                    var chunkHeaderJson = JsonConvert.SerializeObject(f.ChunkHeader, Formatting.Indented);
+                    sb.AppendLine( chunkHeaderJson + ",");
+                    co.WriteLine( $" Scene Chunk Header: {chunkHeaderJson.Data()}");
+                    
+                    
+                    string layerGroupsJson = JsonConvert.SerializeObject(f.LayerGroups, Formatting.Indented);
+                    sb.AppendLine( layerGroupsJson + ",");
+                    co.WriteLine( $" Layer Groups: {layerGroupsJson.Data()}");
+                    
+                    string lgbAssetPaths = JsonConvert.SerializeObject(f.LgbAssetPaths, Formatting.Indented);
+                    sb.AppendLine( lgbAssetPaths + ",");
+                    co.WriteLine( $" LGB Asset Paths: {lgbAssetPaths.Data()}");
+                    
+                    
+                    string layerSetsJson = JsonConvert.SerializeObject(f.LayerSets, Formatting.Indented);
+                    sb.AppendLine( layerSetsJson + ",");
+                    co.WriteLine( $" Layer Sets: {layerSetsJson.Data()}");
+                    
+                    File.WriteAllText(fileName, sb.ToString());
                     break;
                 }
             }
